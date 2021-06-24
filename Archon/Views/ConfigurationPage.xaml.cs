0b76ffe3ed8dfa29406f9385099dffdec1727f35
Henry Pontzer <henry@jpontzer.net>
2021-06-23 18:53:42 -0600

    Post-merge bug fixes

--- Archon/Views/ConfigurationPage.xaml.cs
+++ Archon/Views/ConfigurationPage.xaml.cs
@@ -18,12 +18,12 @@ namespace Archon.Views
             DataContext = ViewModel;
 
             //I want to put this in an async method but I don't know one that won't be called multiple times in the page's lifespan
-            ViewModel.InitializeAsync();
+            //ViewModel.InitializeAsync();
         }
 
         private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            //await ViewModel.InitializeAsync();
+            await ViewModel.InitializeAsync();
             //SettingsCVS.Source = ViewModel.SelectedSettingGroup;
         }
     }
