54e2b43d3e08648680e914c9469612251715aaa5
Henry Pontzer <henry@jpontzer.net>
2021-06-29 12:41:18 -0600

    Added string to severity converter for InfoBar errors

---
+++ Archon/Helpers/StringToSeverityConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Archon.Helpers
+{
+    public class StringToSeverityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string severityValue)
+            {
+                bool valueIsSeverity = Enum.TryParse(typeof(InfoBarSeverity), severityValue, out object severity);
+
+                if (valueIsSeverity)
+                {
+                    return (InfoBarSeverity)severity;
+                }
+                throw new ArgumentException("Value must be an InfoBar severity!");
+            }
+
+            throw new ArgumentException("paramter must be an Enum name!");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString();
+        }
+    }
+}
