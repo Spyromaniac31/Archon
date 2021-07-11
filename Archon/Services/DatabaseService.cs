be74921d2e452fc0e1ae465590b6547a7cfe1642
Henry Pontzer <henry@jpontzer.net>
2021-07-11 15:37:51 -0600

    Updated database service to work with multiple database files

--- Archon/Services/DatabaseService.cs
+++ Archon/Services/DatabaseService.cs
@@ -38,53 +38,20 @@ namespace Archon.Services
         };
         public static SqliteConnection SqliteConnection { get; private set; }
 
-        private static async Task InitializeConnection()
+        private static async Task InitializeConnection(string database)
         {
-            if (SqliteConnection != null)
+            if (SqliteConnection != null && SqliteConnection.DataSource.Contains(database))
             {
                 return;
             }
-            Uri uri = new Uri("ms-appx:///Data/archon.db");
+            Uri uri = new Uri($"ms-appx:///Data/{database}.db");
             StorageFile dbFile = await StorageFile.GetFileFromApplicationUriAsync(uri);
             SqliteConnection = new SqliteConnection($"Filename={dbFile.Path}");
         }
 
-        public static async Task<ObservableCollection<GameSetting>> GetSettingsByCategoryAsync(string category)
-        {
-            await InitializeConnection();
-            var settings = new ObservableCollection<GameSetting>();
-            SqliteConnection.Open();
-
-            //Creates SELECT command using method parameter
-            string commandText = "SELECT * FROM settings WHERE category = $category";
-            var selectCommand = new SqliteCommand(commandText, SqliteConnection);
-            _ = selectCommand.Parameters.AddWithValue("$category", category);
-
-            var dataReader = selectCommand.ExecuteReader();
-
-            //Adds returned records to the ObservableCollection
-            while (dataReader.Read())
-            {
-                settings.Add(new GameSetting()
-                {
-                    Name = !dataReader.IsDBNull(0) ? dataReader.GetString(0) : null,
-                    Description = !dataReader.IsDBNull(1) ? dataReader.GetString(1) : null,
-                    Hint = !dataReader.IsDBNull(2) ? dataReader.GetString(2) : null,
-                    Type = !dataReader.IsDBNull(3) ? dataReader.GetString(3) : null,
-                    Category = !dataReader.IsDBNull(4) ? dataReader.GetString(4) : null,
-                    File = (File)(!dataReader.IsDBNull(5) ? Enum.Parse(typeof(File), dataReader.GetString(5)) : null),
-                    DefaultValue = !dataReader.IsDBNull(6) ? dataReader.GetString(6) : null,
-                });
-            }
-
-            SqliteConnection.Close();
-
-            return settings;
-        }
-
         public static async Task<List<GameSetting>> GetSettings(string table)
         {
-            await InitializeConnection();
+            await InitializeConnection("settings");
             var settings = new List<GameSetting>();
             SqliteConnection.Open();
 
@@ -123,7 +90,5 @@ namespace Archon.Services
                                                select new GroupInfoList(g) { Key = g.Key };
             return new ObservableCollection<GroupInfoList>(query);
         }
-
-
     }
 }
